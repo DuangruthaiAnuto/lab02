41f6516eebc589a908d0e63550a05d61b398ce59
DUANGRUTHAI\Duangruthai <614234005@parichat.skru.ac.th>
2021-01-14 12:26:05 +0700

    สอนการใช้ v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,56 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{ message }}</h1>
+    <br />
+    <span v-text="message"></span>
+
+    <hr />
+    <!-- เส้น -->
+    <h1 v-show="isShow">Hello!!</h1>
+
+    <hr />
+    <!-- เส้น -->
+    <div v-bind:key="emp.id" v-for="emp in employees">
+      <span v-bind:class="(emp.Salary>20000) ? 'high' : (emp.Salary>13000) ? 'medium' : 'low'">
+      {{emp.name}}
+      </span>
+
+      <span v-if="emp.Salary>20000">***</span>
+      <span v-else-if="emp.Salary>13000">**</span>
+      <span v-else>*</span>
+    </div>
+
+    <hr />
+    <!-- เส้น -->
+    <a v-bind:href="urlSKRU">SKRU</a>
+
+    <hr />
+    <!-- เส้น -->
+    <img v-bind:src="require('@/assets/' + pic + '.png')" width="150px" />
+
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: "App",
+  components: {},
+  data() {
+    return {
+      pic:"logo",
+      urlSKRU:"http://www.skru.ac.th",
+      message: "Hello!!",
+      isShow: true,
+      employees: [
+        { "id": 1, "name": "Jinny", "Salary": 15000 },
+        { "id": 2, "name": "Rianny", "Salary": 13000 },
+        { "id": 3, "name": "Plammy", "Salary": 22000 }
+      ],
+    }
+  },
 }
+
 </script>
 
 <style>
@@ -24,5 +61,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.high{
+  color: green;
+}
+.medium{
+  color: rgb(255, 251, 0);
+}
+.low{
+  color: red;
 }
 </style>
